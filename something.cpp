8f2e5512fa2dc79c1b69b09c435ffc4d357631de
KSXGitHub <hvksmr1996@gmail.com>
2015-04-01 12:40:21 +0700

    Create something.cpp

---
+++ something.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	clrscr();
+	cout << "Hello!! :)\n" << "You have pass " << argc - 1 << " parameter(s) to this program\nPress any key to escape...\n";
+	_getch();
+}
