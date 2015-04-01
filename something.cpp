e8c80e530e1491bd66c7f2b08ca85950dbe2416f
KSXGitHub <hvksmr1996@gmail.com>
2015-04-01 12:42:07 +0700

    Update something.cpp

--- something.cpp
+++ something.cpp
@@ -7,4 +7,5 @@ int main(int argc, char* argv[]){
 	clrscr();
 	cout << "Hello!! :)\n" << "You have pass " << argc - 1 << " parameter(s) to this program\nPress any key to escape...\n";
 	_getch();
+	return 0;
 }
